bc237847ac3b4e5ab07830fb3ce13a69e1b972fe
Sami Toukola <toukolasami@gmail.com>
2019-12-12 13:38:31 +0200

    Socialsecuritynumber -input parser

--- hetu/Program.cs
+++ hetu/Program.cs
@@ -6,7 +6,39 @@ namespace hetu
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("ohjeelma tarkistaa hetun");
+            //Console.Write("");
+            string userInput = "101098-593V";
+            int idNumber = InputParse(userInput);
+            isValidID(idNumber);
+
+
+            
+
+        }
+
+
+
+        static int InputParse(string stringParser)
+        {
+            //luetaan
+            string result = stringParser.Substring(0, 5);
+
+            string removed = stringParser.Remove(10, 1);
+            removed = removed.Remove(6, 1);
+
+            return int.Parse(removed);
+        }
+
+        static bool isValidID(int idNumber)
+        {
+            string checkMark = "0123456789ABCDEFHJKLMNPRSTUVWXY";
+
+            int modCheck = idNumber % 31;
+
+            Console.WriteLine(checkMark[modCheck]);
+            return true;
+
         }
     }
 }
