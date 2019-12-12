a42c6fca50e6a67604ea646855a9c70f1a579d4f
Sami Toukola <toukolasami@gmail.com>
2019-12-12 12:56:12 +0200

    Initial commit

---
+++ hetu/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hetu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
