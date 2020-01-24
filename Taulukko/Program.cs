dc60d633af145379dc8d1d3565fd53a8110e553d
Sami Toukola <toukolasami@gmail.com>
2020-01-24 10:45:00 +0200

    Created Taulukko.csproj

---
+++ Taulukko/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Taulukko
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
