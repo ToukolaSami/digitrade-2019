818c0de8a51b7748bfb520311410e8f03bc503fb
Sami Toukola <toukolasami@gmail.com>
2020-01-24 10:59:44 +0200

    Created Merkkijono.csproj

---
+++ Merkkijono/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Merkkijono
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Syötä jotain: ");
+            string a = Console.ReadLine();
+            int i = a.Length;
+            Console.WriteLine($"Syötteessä on {i} merkkiä.");
+
+        }
+    }
+}
