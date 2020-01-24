b75950e958b1459381d56845d6fb29be392a5fad
Sami Toukola <toukolasami@gmail.com>
2020-01-24 12:33:44 +0200

    Finished task3

--- Merkkijono/Program.cs
+++ Merkkijono/Program.cs
@@ -10,8 +10,20 @@ namespace Merkkijono
             string a = Console.ReadLine();
             int i = a.Length;
             Console.WriteLine($"Syötteessä on {i} merkkiä.");
+
             Console.WriteLine("\nSyötteestä on vaihdettu  e-kirjaimet @-merkiksi.");
             Console.WriteLine(a.Replace("e", "@"));
+
+            int n1 = 0;
+            for (int n=0; n<a.Length; n++)
+            {
+                if (a[n]== 'l')
+                {
+                    n1++;
+                }
+            }
+            Console.WriteLine($"\nL kirjaimia syötteessä oli {n1}");
+
 
 
         }
