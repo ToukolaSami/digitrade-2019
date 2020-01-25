e9a71a1f759f8a9f2135ab7f08f3368aea479cee
Sami Toukola <toukolasami@gmail.com>
2020-01-25 11:49:00 +0200

    Finished task1

--- Taulukko/Program.cs
+++ Taulukko/Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 
-namespace Taulukko
+namespace Taulukko1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double[] productPrice = { 7.92, 72.0, 99.90 };
+            int[] productAmount = { 1, 3, 2 };
+            double[] productTotalPrice = { (productPrice[0] * productAmount[0]), (productPrice[1] * productAmount[1]), (productPrice[2] * productAmount[2]) };
+
+            Console.WriteLine($"Product 1: {productTotalPrice[0]}");
+            Console.WriteLine($"Product 2: {productTotalPrice[1]}");
+            Console.WriteLine($"Product 3: {productTotalPrice[2]}");
         }
     }
 }
+
