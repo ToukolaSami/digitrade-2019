2acd3891528218b93ce272cf67f1b90be398130b
Sami Toukola <toukolasami@gmail.com>
2020-01-25 12:02:33 +0200

    Finished task 2

--- Taulukko/Program.cs
+++ Taulukko/Program.cs
@@ -12,7 +12,27 @@ namespace Taulukko1
 
             Console.WriteLine($"Product 1: {productTotalPrice[0]}");
             Console.WriteLine($"Product 2: {productTotalPrice[1]}");
-            Console.WriteLine($"Product 3: {productTotalPrice[2]}");
+            Console.WriteLine($"Product 3: {productTotalPrice[2]}\n");
+      
+            double sum = 0;
+            int n = 0;
+            int[] iT = new int[100];
+            Random rnd = new Random();
+
+            for (int i = 0; i < 100; i++)
+            {
+                iT[n] = rnd.Next(50);
+                n++;
+            }
+            n = 0;
+            for (int i = 0; i < 100; i++)
+            {
+                sum += iT[n];
+                Console.WriteLine($"{n + 1}. {iT[n]}");
+                n++;
+            }
+            Console.WriteLine("");
+            Console.WriteLine($"Ylläolevien lukujen keskiarvo on: {sum / iT.Length}");
         }
     }
 }
