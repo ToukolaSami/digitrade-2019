2051abaa64f541b7af5e7c9523724d3d5274e8e1
Sami Toukola <toukolasami@gmail.com>
2020-01-25 10:31:35 +0200

    Finished task4

--- Merkkijono/Program.cs
+++ Merkkijono/Program.cs
@@ -8,11 +8,13 @@ namespace Merkkijono
         {
             Console.Write("Syötä jotain: ");
             string a = Console.ReadLine();
+            string lowA = a.ToLower();
             int i = a.Length;
-            Console.WriteLine($"Syötteessä on {i} merkkiä.");
+            Console.WriteLine($"\nSyötteessä on {i} merkkiä.");
+            
 
             Console.WriteLine("\nSyötteestä on vaihdettu  e-kirjaimet @-merkiksi.");
-            Console.WriteLine(a.Replace("e", "@"));
+            Console.WriteLine(lowA.Replace("e", "@"));
 
             int n1 = 0;
             for (int n=0; n<a.Length; n++)
@@ -22,10 +24,31 @@ namespace Merkkijono
                     n1++;
                 }
             }
-            Console.WriteLine($"\nL kirjaimia syötteessä oli {n1}");
+            Console.WriteLine($"\nL kirjaimia syötteessä oli {n1}\n");
+            
+            
+            int input = 0;
+            int output = a.Length - 1;
+            bool check = true; 
 
-
-
+            for (int iCheck = 0; iCheck < a.Length - 1; iCheck++)
+            {
+                if (lowA[input] == lowA[output])
+                {
+                    input++;
+                    output--;
+                }
+                else
+                    check = false;
+            }
+            if (check == true)
+            {
+                Console.WriteLine($"{a} on palindromi!");
+            }
+            else
+            {
+                Console.WriteLine($"{a} ei ole palindromi!");
+            }
         }
     }
 }
