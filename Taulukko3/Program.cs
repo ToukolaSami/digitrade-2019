1de0bcf2e43f1189e0026096c523dd3ffc80bf12
Sami Toukola <toukolasami@gmail.com>
2020-01-28 19:42:38 +0200

    Finished task 3.1

--- Taulukko3/Program.cs
+++ Taulukko3/Program.cs
@@ -6,7 +6,18 @@ namespace Taulukko3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Taulukko 3.1\n[X] = [arvo]");
+            int[] numbers = new int[10];
+            Random rnd = new Random();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = rnd.Next(20);
+            }
+            for (int n = 0; n < numbers.Length; n++)
+            {
+                Console.WriteLine($"[{n}] = {numbers[n]:D2}");
+            }
+            Console.ReadKey();
         }
     }
 }
