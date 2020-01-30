b72700d8a1468b3a09781c24484825688c8b837b
Sami Toukola <toukolasami@gmail.com>
2020-01-30 08:10:30 +0200

    Created Harjoitustyö.csproj Started making UserInterface

---
+++ Harjoitustyö/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace Harjoitustyö
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static char UserInterface()
+            {
+            Console.WriteLine("[T] Tarkastaa viitenumeron oikeellisuuden");
+            Console.WriteLine("[L] Luodaan yksi viitenumero");
+            Console.WriteLine("[R] Luodaan haluttu määrä viitenumeroita");
+            Console.WriteLine("[N] Näytä luodut viitenumerot");
+            Console.WriteLine("[X] Lopeta ohjelmansuoritus");
+            Console.Write("Valitse ohjelma: ");
+
+            return char.ToUpper(Console.ReadKey().KeyChar);
+            }
+        }
+    }
+}
